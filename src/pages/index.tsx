1354023dc01ea6c8926954d9be32daf5777cf33c
Anthony Marks <amarks9@austin.rr.com>
2021-09-22 14:07:06 -0500

    Updated site Hero title

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -32,7 +32,7 @@ export default function Page() {
 
       <main className="content">
         <Hero
-          title="Get Started with Headless"
+          title="Anthony Working with Headless"
           buttonText="Developer Docs"
           buttonURL="https://faustjs.org"
           button2Text="Headless on GitHub"
